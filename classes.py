173eb702697c9c0a228eb8bde880f0c781be5edb
Julia Czosnek <julia.czosnek.stud@pw.edu.pl>
2023-12-16 20:59:04 +0100

    added and tested class ValidWords

--- classes.py
+++ classes.py
@@ -23,6 +23,7 @@ class Database:
                     line = line.rstrip()
                     if len(line) == 5 and line.isalpha():
                         self._wordlist.append(line.lower())
+            self._wordlist.sort()
             self._length = len(self._wordlist)
         except FileNotFoundError:
             raise FileNotFoundError(f'File {path} not found')
@@ -36,7 +37,7 @@ class Database:
         return self._length
 
 
-class Guesswords(Database):
+class GuesswordsDatabase(Database):
     '''
     class Guesswords inherits from class Database.
     Contains the same attributes as Database
@@ -50,3 +51,36 @@ class Guesswords(Database):
         '''
         guessword_index = randint(0, super().length - 1)
         return super().wordlist[guessword_index]
+
+
+class ValidWords(Database):
+    '''
+    class ValidWords inherits from class Database.
+    Contains the same attributes as Database
+    '''
+    def __init__(self) -> None:
+        super().__init__('valid_words.txt')
+
+    def binary_search(self, word: str) -> bool:
+        '''
+        Uses binary search algorithm to verify whether
+        <word> is in ValidWords' <wordlist>
+        '''
+        begin = 0
+        end = super().length - 1
+        while begin < end:
+            shoot = (begin + end) / 2
+            shoot = int(shoot)
+            if super().wordlist[shoot] >= word:
+                end = shoot
+            else:
+                begin = shoot + 1
+        return super().wordlist[begin] == word
+
+    def is_valid(self, word: str) -> bool:
+        '''
+        returns True if <word> is in ValidWords' <wordlist>
+        '''
+        if not word or not isinstance(word, str) or len(word) != 5:
+            return False
+        return self.binary_search(word)
--- test_classes.py
+++ test_classes.py
@@ -1,10 +1,14 @@
-from classes import Database, Guesswords
+from classes import Database, GuesswordsDatabase, ValidWords
 import pytest
 
 
 # test class Database
 
 def test_create_database():
+    '''
+    tests init, getting words containng numbers,
+    uppercase or having more or less than 5 letters
+    '''
     database = Database('database_to_test.txt')
     wordlist = ['brand', 'crime', 'pouty']
     assert database.length == 3
@@ -17,10 +21,10 @@ def test_create_database_incorrect_path():
     assert str(excinfo.value) == 'File nonexistent_file.txt not found'
 
 
-# test class Guesswords
+# test class GuesswordsDatabase
 
 def test_create_guesswords():
-    guesswords = Guesswords()
+    guesswords = GuesswordsDatabase()
     assert guesswords.length == 2315
 
 
@@ -28,5 +32,82 @@ def test_generate_guessword(monkeypatch):
     def return_zero(f, t):
         return 0
     monkeypatch.setattr('classes.randint', return_zero)
-    guesswords = Guesswords()
-    assert guesswords.generate_guessword() == 'cigar'
+    guesswords = GuesswordsDatabase()
+    assert guesswords.generate_guessword() == 'aback'
+
+
+# test class ValidWords
+
+def test_create_valid_words():
+    valid_words = ValidWords()
+    assert valid_words.length == 14839
+
+
+def test_binary_search_word_exists():
+    valid_words = ValidWords()
+    assert valid_words.binary_search('abaca') is True
+
+
+def test_binary_search_word_nonexistent():
+    valid_words = ValidWords()
+    assert valid_words.binary_search('caleb') is False
+
+
+def test_binary_search_first_word():
+    valid_words = ValidWords()
+    assert valid_words.binary_search('aahed') is True
+
+
+def test_binary_search_last_word():
+    valid_words = ValidWords()
+    assert valid_words.binary_search('zymic') is True
+
+
+def test_binary_search_odd_index():  # checking for infinite loop
+    valid_words = ValidWords()
+    assert valid_words.binary_search(valid_words.wordlist[1]) is True
+
+
+def test_binary_search_even_index():  # checking for infinite loop
+    valid_words = ValidWords()
+    assert valid_words.binary_search(valid_words.wordlist[7420]) is True
+
+
+def test_binary_search_word_smaller_than_first():
+    valid_words = ValidWords()
+    assert valid_words.binary_search('aaaaa') is False
+
+
+def test_binary_search_word_greater_than_last():
+    valid_words = ValidWords()
+    assert valid_words.binary_search('zzzzz') is False
+
+
+def test_is_valid_for_valid():
+    valid_words = ValidWords()
+    assert valid_words.is_valid('abaca') is True
+
+
+def test_is_valid_for_invalid():
+    valid_words = ValidWords()
+    assert valid_words.is_valid('bruce') is False
+
+
+def test_is_valid_for_none():
+    valid_words = ValidWords()
+    assert valid_words.is_valid(None) is False
+
+
+def test_is_valid_for_numbers_as_str():
+    valid_words = ValidWords()
+    assert valid_words.is_valid('12345') is False
+
+
+def test_is_valid_len_four():
+    valid_words = ValidWords()
+    assert valid_words.is_valid('pick') is False
+
+
+def test_is_valid_len_six():
+    valid_words = ValidWords()
+    assert valid_words.is_valid('banana') is False

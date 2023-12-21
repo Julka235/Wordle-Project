5f1c236b3cc00eb16f216d59f184b882f6370b39
Julia Czosnek <julia.czosnek.stud@pw.edu.pl>
2023-12-22 00:33:44 +0100

    displayed boxes

--- trying_pygame.py
+++ trying_pygame.py
@@ -1,10 +1,23 @@
 import pygame
 
-# size of the displayed window
+# sizes
 WIDTH = 500
-HEIGHT = 500
+HEIGHT = 600
+BOX_SIZE = 72
 
-# init pygame
+# margins
+BIG_MARGIN = 50  # window margin
+BOX_MARGIN = 10  # margin between boxes
+
+# colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+# constant numbers
+TRIES = 6
+WORD_LEN = 5
+
+# initialize pygame
 pygame.init()
 pygame.display.set_caption('Wordle')
 
@@ -13,6 +26,18 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
 show = True
 while show:
+    screen.fill(WHITE)
+
+    # display boxes
+    y = BIG_MARGIN
+    for i in range(TRIES):
+        x = BIG_MARGIN
+        for j in range(WORD_LEN):
+            box = pygame.Rect(x, y, BOX_SIZE, BOX_SIZE)
+            pygame.draw.rect(screen, BLACK, box, width=3)
+            x += BOX_SIZE + BOX_MARGIN
+        y += BOX_SIZE + BOX_MARGIN
+
     # update the screen
     pygame.display.flip()
 

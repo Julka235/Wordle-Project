a93c0c6fb9655b0eac148ed7bbe468fab9de6e81
Julia Czosnek <julia.czosnek.stud@pw.edu.pl>
2024-01-03 18:29:48 +0100

    done some refactoring

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pygame
 from classes import ValidWords, Solution
 
-# constants
+# CONSTANTS
 WIDTH = 500
 HEIGHT = 650
 BOX_SIZE = 53
@@ -13,32 +13,39 @@ BOX_MARGIN = 8  # margin between boxes
 KEY_MARGIN = 3
 WINDOW_KEY_MARGIN = 40
 
-
 WHITE = (255, 255, 255)
 GRAY = (133, 146, 158)
 DARK = (52, 73, 94)
 GREEN = (12, 191, 29)
 YELLOW = (255, 191, 0)
 
-TRIES = 6
+TRIES = 6  # number of tries
 WORD_LEN = 5
 
-FIRST_LETTERS = 'QWERTYUIOP'
-SECOND_LETTERS = 'ASDFGHJKL'
-LAST_LETTERS = 'ZXCVBNM'
+FIRST_LETTERS = 'QWERTYUIOP'  # first keyboard row letters
+SECOND_LETTERS = 'ASDFGHJKL'  # second keyboard row letters
+LAST_LETTERS = 'ZXCVBNM'  # last keyboard row letters
 
+# dict storing what colors should be each letter on the keyboard
 keyboard_colors = {}
 
 # Initialize pygame and create <screen>
 pygame.init()
 pygame.display.set_caption('Wordle')
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+# Initialize fonts
 FONT = pygame.font.SysFont('free sans bold', BOX_SIZE)
 SOLUTION_FONT = pygame.font.SysFont('free sans bold', 35)
 KEY_FONT = pygame.font.SysFont('free sans bold', 25)
 
 
 def display_text(character, color, center_value, font):
+    '''
+        displays <character> on the screen
+        in a box colored <color>
+        and chooses font based on condition with <font>
+    '''
     if font == 'normal':
         letter = FONT.render(character, False, color)
     else:
@@ -48,29 +55,49 @@ def display_text(character, color, center_value, font):
 
 
 def handle_keyboard_colors(input, colors):
+    '''
+        updates <keyboard_colors>
+    '''
     for i in range(5):
         if colors[i] == GRAY:
             colors[i] = DARK
-        con = keyboard_colors.get(input[i], DARK)
-        if con == DARK or con == GRAY:
+
+        current_color = keyboard_colors.get(input[i], DARK)
+        if current_color == DARK or current_color == GRAY:
             keyboard_colors[input[i]] = colors[i]
-        elif con == YELLOW and colors[i] == GREEN:
+        elif current_color == YELLOW and colors[i] == GREEN:
             keyboard_colors[input[i]] = colors[i]
 
 
 class Button:
-    def __init__(self, pos: tuple, measurements: tuple, sign: str) -> None:
+    '''
+    class Button. Contains attributes:
+
+    :param text: rendered given text to <KEY_FONT>
+    :param surface: Rect containing <text>
+    :param area: Rect containing area of the button
+    :param width: width of the button
+    :param height: height of the button
+    :param letter: text displaying on the button as str
+    '''
+    def __init__(self, pos: tuple, measurements: tuple, text: str) -> None:
+        '''
+            Initializes class
+        '''
         x, y = pos
         width, height = measurements
         center_value = (x + width // 2, y + height // 2)
-        self.sign = KEY_FONT.render(sign, False, WHITE)
-        self.surface = self.sign.get_rect(center=center_value)
+        self.text = KEY_FONT.render(text, False, WHITE)
+        self.surface = self.text.get_rect(center=center_value)
         self.area = pygame.Rect(x, y, width, height)
         self.width = width
         self.height = height
-        self.letter = sign
+        self.letter = text
 
     def display(self, color=GRAY):
+        '''
+            displays the button on the screen
+        '''
         x, y = pygame.mouse.get_pos()
         if color == GRAY:
             if self.area.x <= x <= self.area.x + self.width \
@@ -80,19 +107,18 @@ class Button:
                 pygame.draw.rect(screen, GRAY, self.area, border_radius=2)
         else:
             pygame.draw.rect(screen, color, self.area, border_radius=2)
-        screen.blit(self.sign, self.surface)
+        screen.blit(self.text, self.surface)
 
 
 def main():
     # variables
     input = ''
     guessed_words = []
-    game_status = False
+    game_status = False  # not won
 
     # solution
     solution = Solution()
     guessword = solution.guessword
-    print(guessword)
 
     # loop
     showing = True
